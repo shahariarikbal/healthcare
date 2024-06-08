e4afdc2c47e553b38eb43264ceeac7231cd1768d
wiseminds <shahariar@thewiseminds.nl>
2024-06-08 17:26:25 +0600

    fix:in admin dashboard total count

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Appointment;
+use App\Models\Doctor;
+use App\Models\Patient;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +27,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.home.index');
+        $totalAppointments = Appointment::select(['id'])->get()->count();
+        $todayAppointments = Appointment::where('appointment_date', Carbon::today())->select(['id'])->get()->count();
+        $patients = Patient::select(['id'])->get()->count();
+        $doctors = Doctor::select(['id'])->get()->count();
+        return view('admin.home.index', compact(['totalAppointments', 'todayAppointments', 'patients', 'doctors']));
     }
 }
--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -8,7 +8,7 @@
                       <div class="d-flex">
                           <div class="flex-grow-1">
                               <p class="text-truncate mb-2 text-white">Total Appointment</p>
-                              <h4 class="mb-2 text-white">1452</h4>
+                              <h4 class="mb-2 text-white">{{ $totalAppointments ?? 0 }}</h4>
                           </div>
                           <div class="avatar-sm">
                               <span class="avatar-title rounded-3">
@@ -25,7 +25,7 @@
                       <div class="d-flex">
                           <div class="flex-grow-1">
                               <p class="text-truncate mb-2 text-white">Today Appointment</p>
-                              <h4 class="mb-2 text-white">938</h4>
+                              <h4 class="mb-2 text-white">{{ $todayAppointments ?? 0 }}</h4>
                           </div>
                           <div class="avatar-sm">
                               <span class="avatar-title rounded-3">
@@ -41,12 +41,12 @@
                   <div class="card-body">
                       <div class="d-flex">
                           <div class="flex-grow-1">
-                              <p class="text-truncate mb-2 text-white">Departments</p>
-                              <h4 class="mb-2 text-white">8246</h4>
+                              <p class="text-truncate mb-2 text-white">Patients</p>
+                              <h4 class="mb-2 text-white">{{ $patients ?? 0 }}</h4>
                           </div>
                           <div class="avatar-sm">
                               <span class="avatar-title rounded-3">
-                                  <i class="fa-solid fa-graduation-cap icon"></i>
+                                <i class="fa-solid fa-bed-pulse"></i>
                               </span>
                           </div>
                       </div>                                              
@@ -59,7 +59,7 @@
                       <div class="d-flex">
                           <div class="flex-grow-1">
                               <p class="text-truncate mb-2 text-white">Doctors</p>
-                              <h4 class="mb-2 text-white">29670</h4>
+                              <h4 class="mb-2 text-white">{{ $doctors ?? 0 }}</h4>
                           </div>
                           <div class="avatar-sm">
                               <span class="avatar-title rounded-3">
