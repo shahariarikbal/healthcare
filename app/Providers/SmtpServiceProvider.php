4f577489cbd65115e0f0801f8d28dc254bf96c6c
wiseminds <shahariar@thewiseminds.nl>
2024-06-09 21:16:45 +0600

    fix:Update smtp provider

--- app/Providers/SmtpServiceProvider.php
+++ app/Providers/SmtpServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\SmtpSetting;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class SmtpServiceProvider extends ServiceProvider
@@ -21,23 +22,25 @@ class SmtpServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $setting = SmtpSetting::first();
+        if(Schema::hasTable('smtp_settings')){
+            $setting = SmtpSetting::first();
 
-        if($setting){
-            $config = [
-                'driver' => $setting->mail_mailer,
-                'host' => $setting->mail_host,
-                'port' => $setting->mail_port,
-                'username' => $setting->mail_username,
-                'password' => $setting->mail_password,
-                'encryption' => $setting->mail_encryption,
-                'from' => [
-                    'address' => $setting->mail_from_address,
-                    'name' => $setting->mail_from_name,
-                ],
-            ];
-
-            Config::set('mail', $config);
+            if($setting){
+                $config = [
+                    'driver' => $setting->mail_mailer,
+                    'host' => $setting->mail_host,
+                    'port' => $setting->mail_port,
+                    'username' => $setting->mail_username,
+                    'password' => $setting->mail_password,
+                    'encryption' => $setting->mail_encryption,
+                    'from' => [
+                        'address' => $setting->mail_from_address,
+                        'name' => $setting->mail_from_name,
+                    ],
+                ];
+    
+                Config::set('mail', $config);
+            }
         }
     }
 }
